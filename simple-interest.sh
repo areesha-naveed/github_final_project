4198899b120cb233d09988b85c113acad63c9daa
areesha-naveed <areeshanaveed295@gmail.com>
2026-08-03 19:08:14 +0500

    simple-intesrest.sh
    Added user input prompts for principal, time, and rate of interest. Calculated and displayed simple interest based on user inputs.

--- simple-interest.sh
+++ simple-interest.sh
@@ -7,3 +7,14 @@
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
 # areesha-naveed
+
+echo "Enter the principal:"
+read p
+echo "Enter time period in years:"
+read t
+echo "Enter rate of interest per year:"
+read r
+
+s=$(echo "scale=2; $p * $t * $r / 100" | bc)
+echo "The simple interest is: "
+echo $s
